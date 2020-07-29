8f0f7380ef295c69e412c73fa08968defe72798c
Digginss <68953188+Digginss@users.noreply.github.com>
2020-07-29 20:35:47 +0200

    Add files via upload

--- quizzapp.py
+++ quizzapp.py
@@ -15,15 +15,57 @@ def question_1_1():
 
 def question_1_2():
     import os
+    os.chdir(r'C:\Users\majab\Desktop\QUizapp\QuizApp-questions\QuizApp-questions\questions')
+    filename = open("1.txt","r")
+    print(filename.read())
     os.chdir(r'C:\Users\majab\Desktop\QUizapp\QuizApp-answers\QuizApp-answers\answers')
     cwd = os.getcwd()
-    filename = '1.txt'
+    filename = open("1.txt","r")
+    print(filename.read())
+    filename = "1.txt"
+    filename.split("/n")
     with open(filename) as f_obj:
-        answers = f_obj.read()
+        answers = f_obj.read(1)
         answer_to_check = input("Your answer: ")
         if answer_to_check in answers:
+            
             print("You are right!")
         else:
             print("You are wrong :c")
 
-question_1_2()
+
+
+def whers_star():
+      import os
+      os.chdir(r'C:\Users\majab\Desktop\QUizapp\QuizApp-questions\QuizApp-questions\questions')
+      filename = open("1.txt","r")
+      print(filename.read())
+      os.chdir(r'C:\Users\majab\Desktop\QUizapp\QuizApp-answers\QuizApp-answers\answers')
+      filename = open("1.txt","r")
+      print(filename.read())
+      filename = "1.txt"
+      filename.splitlines("/n")
+      with open(filename) as f_obj:
+        answer_to_check = input("Your answer: ")
+        if "*" in f_obj.readline():
+             if answer_to_check in f_obj.readlines(1):
+                 print("You are right!")
+             else:
+                 print("You are wrong!")
+        elif "*" in f_obj.readlines(2):
+             if answer_to_check in f_obj.readlines(2):
+                 print("You are right!")
+             else:
+                 print("You are wrong!")
+        elif "*" in f_obj.readlines(3):
+             if answer_to_check in f_obj.readlines(3):
+                 print("You are right!")
+             else:
+                 print("You are wrong!")
+        else:
+            if answer_to_check in f_obj.readlines(4):
+                 print("You are right!")
+            else:
+                 print("You are wrong!")
+
+whers_star() 
