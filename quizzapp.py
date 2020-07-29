b499bda5ec577672d99339f764982e8743b420d7
Digginss <68953188+Digginss@users.noreply.github.com>
2020-07-29 18:38:32 +0200

    Add files via upload

--- quizzapp.py
+++ quizzapp.py
@@ -1,7 +1,7 @@
 user_points = []
 
 #Question 1
-def question_1():
+def question_1_1():
     print("Humans and chimpanzees share roughly how much DNA?")
     print(" A) 70% \n B) 98% \n C) 9% \n D) 38%")
     user_answer=input("Your answer: ")
@@ -13,4 +13,17 @@ def question_1():
             user_points.append(0)
 
 
-question_1()
+def question_1_2():
+    import os
+    os.chdir(r'C:\Users\majab\Desktop\QUizapp\QuizApp-answers\QuizApp-answers\answers')
+    cwd = os.getcwd()
+    filename = '1.txt'
+    with open(filename) as f_obj:
+        answers = f_obj.read()
+        answer_to_check = input("Your answer: ")
+        if answer_to_check in answers:
+            print("You are right!")
+        else:
+            print("You are wrong :c")
+
+question_1_2()
