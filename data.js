c8c90fd37e71e94ea6f9d026855dfc9364350025
evacuati <66165976+evacuati@users.noreply.github.com>
2020-07-31 09:19:27 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,93 @@
+data = 
+[
+	{
+		"question": "Humans and chimpanzees share roughly how much DNA?",
+		"answers": [
+			{answer: "98%", isCorrect: true},
+			{answer: "75%", isCorrect: false},
+			{answer: "23%", isCorrect: false},
+			{answer: "38%", isCorrect: false},
+		]	
+	},
+	{
+		"question": "What is the most abundant gas in the Earth’s atmosphere?",
+		"answers" : [
+			{answer: "Hydrogen", isCorrect: false},
+			{answer: "Methane", isCorrect: false},
+			{answer: "Nitrogen", isCorrect: true},
+			{answer: "Argon", isCorrect: false},
+		]	
+	},
+	{
+		"question": "Roughly how long does it take for the sun’s light to reach Earth?",
+		"answers" : [
+			{answer: "8 minutes", isCorrect: true},
+			{answer: "17 minutes", isCorrect: false},
+			{answer: "2 days", isCorrect: false},
+			{answer: "4 hours", isCorrect: false},
+		]
+	},
+	{
+		"question": "Which famous British physicist wrote A Brief History of Time?",
+		"answers" : [
+			{answer: "Isaac Newton", isCorrect: false},
+			{answer: "Stephen Hawking", isCorrect: true},
+			{answer: "James Clerk Maxwell", isCorrect: false},
+			{answer: "Rudolf Peierls", isCorrect: false},
+		]
+	},
+	{
+		"question": "At what temperature are Celsius and Fahrenheit equal?",
+		"answers" : [
+			{answer: "0°", isCorrect: false},
+			{answer: "-100°", isCorrect: false},
+			{answer: "40°", isCorrect: false},
+			{answer: "-40°", isCorrect: true},
+		]
+	},
+	{
+		"question": "What modern-day country was Marie Curie born in?",
+		"answers" : [
+			{answer: "Italy", isCorrect: false},
+			{answer: "France", isCorrect: false},
+			{answer: "Poland", isCorrect: true},
+			{answer: "England", isCorrect: false},
+		]
+	},
+	{
+		"question": "What is the biggest planet in our solar system?",
+		"answers" : [
+			{answer: "Saturn", isCorrect: false},
+			{answer: "Jupiter", isCorrect: true},
+			{answer: "Earth", isCorrect: false},
+			{answer: "Venus", isCorrect: false},
+		]
+	},
+	{
+		"question": "What name is given for the number of protons found in the nucleus of an atom?",
+		"answers" : [
+			{answer: "Atomic Number", isCorrect: true},
+			{answer: "Inrow Number", isCorrect: false},
+			{answer: "Molecular Number", isCorrect: false},
+			{answer: "Proton Number", isCorrect: false},
+		]
+	},
+	{
+		"question": "How many vertebrae does the average human possess?",
+		"answers" : [
+			{answer: "35", isCorrect: false},
+			{answer: "27", isCorrect: false},
+			{answer: "40", isCorrect: false},
+			{answer: "33", isCorrect: true},
+		]
+	},
+	{
+		"question": "What was the name of the first man-made satellite launched by the Soviet Union in 1957?",
+		"answers" : [
+			{answer: "Buran", isCorrect: false},
+			{answer: "Polyus", isCorrect: false},
+			{answer: "Concorde", isCorrect: true},
+			{answer: "Sputnik", isCorrect: false},
+		]
+	},
+]
